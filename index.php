3c1f66f7f211083846e94d6beb2b3d1f2ba1b933
subbylee <olasubomi.esan@st.vvu.edu.gh>
2016-11-28 05:27:01 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<form action="login.php" method="post">
+<span>Username:</span><input type="text" name="username" /><br><br>
+<span>Password :</span><input type="password" name="password" /><br><br>
+<span style="display: none;">Type :</span>
+<select name="asas" style="display: none;">
+<option>Student</option>
+</select>
+<span>&nbsp;</span><span style="font-size: 11px; font-weight: normal; width: 170px; text-align: left;">Not yet a member? Register <a rel="facebox" href="register.php">Here</a><br>Forgot Password? click <a href="#" onClick="MyWindow=window.open('pwordrecover.php','MyWindow','toolbar=no,location=no,directories=yes,status=yes,menubar=yes,scrollbars=yes,resizable=yes,width=300,height=200'); return false;">Here</a></span><br><br>
+<span>&nbsp;</span><input id="btn" type="submit" value="Login" />
+</form>
+</div>
+</body>
+</html>
