1897db9f2fa188c892dff76e5ef786b408c37f0a
subbylee <olasubomi.esan@st.vvu.edu.gh>
2016-12-13 00:30:29 +0000

    Add files via upload

---
+++ admin/addadmin.php
@@ -0,0 +1,7 @@
+<form action="saveadmin.php" method="POST">
+Username<br>
+<input type="text" name="username" value="" /><br>
+Password<br>
+<input type="text" name="password" value="" /><br>
+<input type="submit" value="Save" />
+</form>
---
+++ admin/addidnum.php
@@ -0,0 +1,12 @@
+<form action="saveidnum.php" method="POST">
+ID Number<br>
+<input type="text" name="idnum" value="" required="required" /><br>
+Firstname<br>
+<input type="text" name="fname" value="" required="required" /><br>
+Lastname<br>
+<input type="text" name="lname" value="" required="required" /><br>
+Middlename<br>
+<input type="text" name="mname" value="" /><br>
+</select><br>
+<input type="submit" value="Save" />
+</form>
